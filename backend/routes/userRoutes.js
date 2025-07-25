8a4c7de8ecb86b2fcd1858209c74ccd41caa998f
lakshrajkumar26 <lakshrajkumar.791.lrk@gmail.com>
2025-07-26 01:33:28 +0530

    final push after role initailized working with /user /admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,16 @@
 const express = require("express");
- 
+const {verifyToken} = require("../middlewares/authMiddleware");
+const {authorizeRoles} = require("./../middlewares/roleMiddleware");
 const router = express.Router(); 
 
-router.get("/user" , (req,res) => {
-    res.json({message : "Welcome User"});
+router.get("/user" , verifyToken , authorizeRoles("admin" , "manager" , "user") , (req,res) => {    // this admin , manager ,user all can access 
+    res.status(200).json({message : "Welcome User"});
 })
-router.get("/manager" , (req,res) => {
-    res.json({message : "Welcome Manager"});
+router.get("/manager" ,verifyToken , authorizeRoles("admin" , "manager") , (req,res) => {
+    res.status(200).json({message : "Welcome Manager"});
 })
-router.get("/admin" , (req,res) => {
-    res.json({message : "Welcome Admin"});
+router.get("/admin" ,verifyToken, authorizeRoles("admin") , (req,res) => {
+    res.status(200).json({message : "Welcome Admin"});
 })
 
 module.exports = router;
