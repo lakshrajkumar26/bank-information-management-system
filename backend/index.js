133c0f505a8803846024b5e07678e49b31db6f84
lakshrajkumar26 <lakshrajkumar.791.lrk@gmail.com>
2025-07-28 18:36:22 +0530

    v2 rename filename

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT =  process.env.PORT || 3000;
 const user = require("./models/User");
 const db = require('./config/dbConnection');
 const authRoutes = require('./routes/authRoutes');
-const userRouters = require('./routes/UserRoutes');
+const userRoutes = require('./routes/userRoutes'); //
 const bankRoutes = require("./routes/bankRoutes");
 const adminRoutes = require('./routes/adminRoutes');
 
@@ -22,7 +22,7 @@ app.get("/health", (req,res)=>{
 })
 
 app.use('/api/auth',authRoutes);
-app.use('/api/users',userRouters);
+app.use('/api/users',userRoutes);
 app.use("/api/user", bankRoutes);
 
 
